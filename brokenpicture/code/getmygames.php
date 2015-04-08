56f87cc0d98a3bb7b8bd72cdbb765d715f1db4ae
jim <jmange@gmail.com>
2015-04-07 22:12:49 -0400

    cleanup

--- brokenpicture/code/getmygames.php
+++ brokenpicture/code/getmygames.php
@@ -5,14 +5,9 @@
 
 if ($user instanceof user) {
     $user_id = $user->getId();
-//    $nextplayer = $_POST['player'];
-    //session_start();
-    //$greeting = "Welcome, <a href=\"#\" class=\"dropdown-toggle\" data-toggle=\"dropdown\" role=\"button\" aria-expanded=\"false\">" . $user->getEmail() . "</a>";
 } else {
-   // $greeting = "<a href=\"login.php\">Log In</a> / <a href=\"signup.php\">Sign Up</a>";
-   // require_once 'notloggedin.php';
-  echo "Not logged in.";
-  exit;
+    echo "Not logged in.";
+    exit;
 }
 
 $games = game::get_user_games($user->getId());
@@ -30,28 +25,3 @@ foreach ($games as $game) {
         }
     }
 }
-
-
-
-// $conn = dbconn::getInstance();
-// //echo $user->getId();
-
-// $sql = "select idhash from turns where receiver = " . $user->getId() . " and status = 0";
-// $stmt = $conn->prepare($sql);
-// $stmt->execute();
-// //echo "testing";
-// while ($row = $stmt->fetch()) {
-//     echo "<div><a href='play.php?id=" . $row['idhash'] . "'</a>" . $row['idhash'] . "<span class=\"label label-info\">New</span></div>";
-// }
-
-// $sql = "select id,idhash, game, max(status) as gamestatus from turns where player = " . $user->getId() . " group by game order by gamestatus";
-// $stmt = $conn->prepare($sql);
-// $stmt->execute();
-// while ($row = $stmt->fetch()) {
-//     if ($row['gamestatus'] == 2) {
-//         echo "<div><a href='play.php?id=" . $row['idhash'] . "'</a>" . $row['idhash'] . "<span class=\"label label-success\">Finished</span></div>";
-//     } else {
-//         echo "<div><a href='play.php?id=" . $row['idhash'] . "'</a>" . $row['idhash'] . "</div>";
-//     }
-    
-// }
